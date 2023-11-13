f8deb9b90b716d47cecf9acd171e5000f11b175c
Rishabh Shukla <rs515666@gmail.com>
2023-11-13 14:17:50 +0530

    Added new files

--- Decision Tree.R
+++ Decision Tree.R
@@ -3,17 +3,17 @@ library(rpart)
 library(rpart.plot)
 
 # Load the Iris dataset
-data <- read.csv("Titanic.csv", stringsAsFactors = FALSE)
+data(iris)
 
 # Create a decision tree model
 # rpart(WHAT TO PREDICT ~ INPUT COLUMNS(+), data = DATASET)
-tree <- rpart(Survived ~ Sex, data = data)
+tree <- rpart(Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width, data = iris)
 
 # Plot the decision tree using rpart.plot
 rpart.plot(tree, main = "Decision Tree", under = TRUE, tweak = 1.2)
 
 # Make predictions on new data
-new_data <- data.frame(Pclass = 2, Sex = "female", Age = 20, SibSp = 0, Parch = 1, Fare = 30, Embarked = "S")
+new_data <- data.frame(Sepal.Length = 5.0, Sepal.Width = 3.5, Petal.Length = 1.5, Petal.Width = 0.2)
 prediction <- predict(tree, new_data, type = "class")
 
 # Display the predicted class
